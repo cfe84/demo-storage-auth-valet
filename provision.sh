0f43f866fd6eb7c9652af1925a9785a2272d121f
Charles Feval <chfeval@microsoft.com>
2019-04-12 18:22:36 -0700

    Fixing provisioning script

--- provision.sh
+++ provision.sh
@@ -33,6 +33,7 @@ do
 done
 
 RANDOMBASE="`random 5`"
+BASENAME="$NAME-$RANDOMBASE"
 STORAGEBASENAME="`echo -n $NAME | head -c 15`$RANDOMBASE"
 SUBSCRIPTIONID="`az account show --query id -o tsv`"
 SUBSCRIPTION_RESOURCE_ID="/subscriptions/$SUBSCRIPTIONID"
@@ -45,14 +46,14 @@ echo "ResourceGroup (default)"
 echo "StorageAccount (default)"
 echo "FunctionApp (default)"
 
-DEFAULT_APPLICATION_IDENTIFIER_URI="https://$NAME-`random 5`"
-DEFAULT_APPLICATION_PASSWORD="@`random 16`!#123001"
-DEFAULT_RESOURCE_GROUP="$NAME"
+DEFAULT_APPLICATION_PASSWORD="`random 16`#123001"
+DEFAULT_RESOURCE_GROUP="$BASENAME"
 DEFAULT_RESOURCE_GROUP_RESOURCE_ID="$SUBSCRIPTION_RESOURCE_ID/resourceGroups/$DEFAULT_RESOURCE_GROUP"
 DEFAULT_STORAGE_ACCOUNT="`echo "$STORAGEBASENAME" | sed -e 's/-//g' | sed -E 's/^(.*)$/\L\1/g' | head -c 20`def"
 DEFAULT_STORAGE_ACCOUNT_RESOURCE_ID="$DEFAULT_RESOURCE_GROUP_RESOURCE_ID/providers/Microsoft.Storage/storageAccounts/$DEFAULT_STORAGE_ACCOUNT"
-DEFAULT_FUNCTIONAPP="$NAME-`random 5`"
+DEFAULT_FUNCTIONAPP="$BASENAME"
 DEFAULT_FUNCTIONAPP_HOSTNAME="https://$DEFAULT_FUNCTIONAPP.azurewebsites.net"
+DEFAULT_APPLICATION_IDENTIFIER_URI="http://$BASENAME.microsoft.com"
 
 echo "Creating App Registration $DEFAULT_APPLICATION_IDENTIFIER_URI"
 echo "[{
@@ -62,7 +63,7 @@ echo "[{
         'type': 'Scope'
         } ]
     }]" > app-registration-manifest.tmp.json
-DEFAULT_APPLICATION_ID=`az ad app create --identifier-uris $DEFAULT_APPLICATION_IDENTIFIER_URI --available-to-other-tenants true --reply-urls $DEFAULT_FUNCTIONAPP_HOSTNAME/.auth/login/aad/callback --display-name DEFAULT_APPLICATION_IDENTIFIER_URI --password "$DEFAULT_APPLICATION_PASSWORD" --required-resource-access app-registration-manifest.tmp.json --query "appId" -o tsv`
+DEFAULT_APPLICATION_ID=`az ad app create --identifier-uris $DEFAULT_APPLICATION_IDENTIFIER_URI --available-to-other-tenants true --reply-urls $DEFAULT_FUNCTIONAPP_HOSTNAME/.auth/login/aad/callback --display-name $DEFAULT_APPLICATION_IDENTIFIER_URI --password "$DEFAULT_APPLICATION_PASSWORD" --required-resource-access app-registration-manifest.tmp.json --query "appId" -o tsv`
 echo "Creating resource group $DEFAULT_RESOURCE_GROUP"
 az group create --name $DEFAULT_RESOURCE_GROUP --location $LOCATION --query "properties.provisioningState" -o tsv
 echo "Creating storage account $DEFAULT_STORAGE_ACCOUNT"
@@ -75,11 +76,11 @@ az storage container create --name "files" --account-name $DEFAULT_STORAGE_ACCOU
 echo "Creating functionapp $DEFAULT_FUNCTIONAPP"
 az functionapp create -g $DEFAULT_RESOURCE_GROUP --consumption-plan-location $LOCATION --name $DEFAULT_FUNCTIONAPP --storage-account $DEFAULT_STORAGE_ACCOUNT --query "state" -o tsv
 az functionapp config appsettings set --name $DEFAULT_FUNCTIONAPP -g $DEFAULT_RESOURCE_GROUP --settings STORAGE_CONNECTION_STRING=$DEFAULT_STORAGE_ACCOUNT_CONNECTION_STRING > /dev/null
-echo "Configuring easy auth for functionapp $DEFAULT_FUNCTIONAPP"
+echo "
+Configuring easy auth for functionapp $DEFAULT_FUNCTIONAPP"
 az webapp auth update --ids $DEFAULT_RESOURCE_GROUP_RESOURCE_ID/providers/Microsoft.Web/sites/$DEFAULT_FUNCTIONAPP --action LoginWithAzureActiveDirectory --enabled true --aad-client-id $DEFAULT_APPLICATION_ID --aad-client-secret "$DEFAULT_APPLICATION_PASSWORD" --aad-token-issuer-url https://login.microsoftonline.com/$TENANTID/  > /dev/null
 echo "Deploying function app $DEFAULT_FUNCTIONAPP"
 func azure functionapp publish $DEFAULT_FUNCTIONAPP
-
 
 echo "Generating cleanup script"
 echo "#!/bin/bash
